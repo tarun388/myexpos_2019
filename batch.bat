ddb5c5511eae766b0f52a579ca4127d7fa0366e1
boruto <tarunverma341998@gmail.com>
2019-09-01 15:46:45 +0530

    STAGE 16 COMPLETED

--- batch.bat
+++ batch.bat
@@ -1,11 +1,11 @@
 load --os $HOME/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/myexpos/expl/expl_progs/even.xsm
-load --exec $HOME/myexpos/expl/expl_progs/odd.xsm
-load --exec $HOME/myexpos/expl/expl_progs/prime.xsm
+load --init $HOME/myexpos/expl/expl_progs/bubble_sort.xsm
 load --idle $HOME/myexpos/expl/expl_progs/idle.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
 load --library $HOME/myexpos/expl/library.lib
+load --int=console $HOME/myexpos/spl/spl_progs/console.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=6 $HOME/myexpos/spl/spl_progs/INT_6.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
