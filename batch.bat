98e7167540d8e8bc4bef507436f02c437581949c
boruto <tarunverma341998@gmail.com>
2019-09-05 01:10:02 +0530

    STAGE 17 COMPLETED

--- batch.bat
+++ batch.bat
@@ -1,5 +1,5 @@
 load --os $HOME/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/myexpos/expl/expl_progs/bubble_sort.xsm
+load --init $HOME/myexpos/expl/expl_progs/shell_v1.xsm
 load --idle $HOME/myexpos/expl/expl_progs/idle.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/haltprog.xsm
 load --library $HOME/myexpos/expl/library.lib
@@ -7,8 +7,12 @@ load --int=console $HOME/myexpos/spl/spl_progs/console.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/INT_6.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
+load --int=9 $HOME/myexpos/spl/spl_progs/INT_9.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
+load --module 1 $HOME/myexpos/spl/spl_progs/mod_1.xsm
+load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
 load --module 4 $HOME/myexpos/spl/spl_progs/mod_4.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/mod_5.xsm
 load --module 7 $HOME/myexpos/spl/spl_progs/mod_7.xsm
+load --exec $HOME/myexpos/expl/expl_progs/odd.xsm
