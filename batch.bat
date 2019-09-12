c975faa70322dc94b291ee528496542418cd2bd2
boruto <tarunverma341998@gmail.com>
2019-09-12 11:14:21 +0530

    STAGE 20 COMPLETED and Fixed Bugs in previous stages. Arguments passing in module calls corrected.

--- batch.bat
+++ batch.bat
@@ -1,5 +1,5 @@
 load --os $HOME/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/myexpos/expl/expl_progs/shell_v1.xsm
+load --init $HOME/myexpos/expl/expl_progs/stage20_assg2.xsm
 load --idle $HOME/myexpos/expl/expl_progs/idle.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/exception_handler.xsm
 load --library $HOME/myexpos/expl/library.lib
@@ -8,6 +8,7 @@ load --int=console $HOME/myexpos/spl/spl_progs/console.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/INT_6.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
+load --int=8 $HOME/myexpos/spl/spl_progs/INT_8.xsm
 load --int=9 $HOME/myexpos/spl/spl_progs/INT_9.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
@@ -16,7 +17,9 @@ load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
 load --module 4 $HOME/myexpos/spl/spl_progs/mod_4.xsm
 load --module 5 $HOME/myexpos/spl/spl_progs/mod_5.xsm
 load --module 7 $HOME/myexpos/spl/spl_progs/mod_7.xsm
+load --exec $HOME/myexpos/expl/expl_progs/even.xsm
+load --exec $HOME/myexpos/expl/expl_progs/odd.xsm
 load --exec $HOME/myexpos/expl/expl_progs/llist.xsm
 load --exec $HOME/myexpos/expl/expl_progs/llist_1.xsm
-load --exec $HOME/myexpos/expl/expl_progs/prime.xsm
-load --exec $HOME/myexpos/expl/expl_progs/gcd.xsm
+load --exec $HOME/myexpos/expl/expl_progs/assg2.xsm
+load --exec $HOME/myexpos/expl/expl_progs/bsort.xsm
