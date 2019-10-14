f67df5a5861448e84403f659104c1fe62252f9b4
boruto <tarunverma341998@gmail.com>
2019-10-15 01:15:16 +0530

    Stage 23 completed

--- batch.bat
+++ batch.bat
@@ -6,6 +6,7 @@ load --library $HOME/myexpos/expl/library.lib
 load --int=disk $HOME/myexpos/spl/spl_progs/disk.xsm
 load --int=console $HOME/myexpos/spl/spl_progs/console.xsm
 load --int=timer $HOME/myexpos/spl/spl_progs/sample_timer.xsm
+load --int=4 $HOME/myexpos/spl/spl_progs/INT_4.xsm
 load --int=6 $HOME/myexpos/spl/spl_progs/INT_6.xsm
 load --int=7 $HOME/myexpos/spl/spl_progs/sample_int7.xsm
 load --int=8 $HOME/myexpos/spl/spl_progs/INT_8.xsm
@@ -14,7 +15,7 @@ load --int=10 $HOME/myexpos/spl/spl_progs/int_10.xsm
 load --int=11 $HOME/myexpos/spl/spl_progs/INT_11.xsm
 load --int=13 $HOME/myexpos/spl/spl_progs/INT_13.xsm
 load --int=14 $HOME/myexpos/spl/spl_progs/INT_14.xsm
-load --int=15 $HOME/myexpos/spl/spl_progs/haltprog.xsm
+load --int=15 $HOME/myexpos/spl/spl_progs/INT_15.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
 load --module 1 $HOME/myexpos/spl/spl_progs/mod_1.xsm
 load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
@@ -27,3 +28,6 @@ load --exec $HOME/myexpos/expl/expl_progs/parent.xsm
 load --exec $HOME/myexpos/expl/expl_progs/s22_a1.xsm
 load --exec $HOME/myexpos/expl/expl_progs/s21_a2.xsm
 load --exec $HOME/myexpos/expl/expl_progs/ms_c.xsm
+load --exec $HOME/myexpos/expl/expl_progs/pid.xsm
+load --exec $HOME/myexpos/expl/expl_progs/s23_a1.xsm
+load --exec $HOME/myexpos/expl/expl_progs/s23_a2.xsm
