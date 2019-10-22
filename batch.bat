02746ef02f6808d32c502bd1dfd50f4382572979
boruto <tarunverma341998@gmail.com>
2019-10-23 02:45:56 +0530

    Stage 26 complete

--- batch.bat
+++ batch.bat
@@ -1,6 +1,7 @@
 load --os $HOME/myexpos/spl/spl_progs/final_os_startup.xsm
-load --init $HOME/myexpos/expl/expl_progs/extended_shell.xsm
+load --init $HOME/myexpos/expl/expl_progs/login.xsm
 load --idle $HOME/myexpos/expl/expl_progs/idle.xsm
+load --shell $HOME/myexpos/expl/expl_progs/extended_shell.xsm
 load --exhandler $HOME/myexpos/spl/spl_progs/exception_handler.xsm
 load --library $HOME/myexpos/expl/library.lib
 load --int=disk $HOME/myexpos/spl/spl_progs/disk.xsm
@@ -14,9 +15,12 @@ load --int=8 $HOME/myexpos/spl/spl_progs/INT_8.xsm
 load --int=9 $HOME/myexpos/spl/spl_progs/INT_9.xsm
 load --int=10 $HOME/myexpos/spl/spl_progs/int_10.xsm
 load --int=11 $HOME/myexpos/spl/spl_progs/INT_11.xsm
+load --int=12 $HOME/myexpos/spl/spl_progs/INT_12.xsm
 load --int=13 $HOME/myexpos/spl/spl_progs/INT_13.xsm
 load --int=14 $HOME/myexpos/spl/spl_progs/INT_14.xsm
 load --int=15 $HOME/myexpos/spl/spl_progs/INT_15.xsm
+load --int=16 $HOME/myexpos/spl/spl_progs/INT_16.xsm
+load --int=17 $HOME/myexpos/spl/spl_progs/INT_17.xsm
 load --module 0 $HOME/myexpos/spl/spl_progs/mod_0.xsm
 load --module 1 $HOME/myexpos/spl/spl_progs/mod_1.xsm
 load --module 2 $HOME/myexpos/spl/spl_progs/mod_2.xsm
@@ -28,6 +32,8 @@ load --exec $HOME/myexpos/expl/expl_progs/ls.xsm
 load --exec $HOME/myexpos/expl/expl_progs/rm.xsm
 load --exec $HOME/myexpos/expl/expl_progs/cp.xsm
 load --exec $HOME/myexpos/expl/expl_progs/cat.xsm
+load --exec $HOME/myexpos/expl/expl_progs/lu.xsm
+load --exec $HOME/myexpos/expl/expl_progs/ru.xsm
 load --exec $HOME/myexpos/expl/expl_progs/s25_a1.xsm
 load --exec $HOME/myexpos/expl/expl_progs/s25_a2.xsm
 load --exec $HOME/myexpos/expl/expl_progs/s25_a3.xsm
