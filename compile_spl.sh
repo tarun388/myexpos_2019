85ae60c6a0a670b5e7fd15e9f8174be089188647
boruto <tarunverma341998@gmail.com>
2019-10-26 22:44:15 +0530

    Stage 27 Completed

--- compile_spl.sh
+++ compile_spl.sh
@@ -7,6 +7,15 @@ cd ..
 for i in $files
 do
 	./spl spl_progs/$i
-	echo "Done compiling $i..."
+	echo $i
+done
+cd ../expl/expl_progs/
+files=$(ls *.expl)
+cd ..
+for i in $files
+do
+	./expl expl_progs/$i
+	echo $i
 done
 cd $curr
+echo "All files compiled successfully."
